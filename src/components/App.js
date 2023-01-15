d7e4c14fe585123cf83804b5f5ae24076908e270
Sammy-CK <sammycherono2696@gmail.com>
2023-01-15 10:09:00 +0300

    loaded Img using useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,36 @@
 // create your App component here
+import React, {useState, useEffect} from 'react';
+
+
+function App(){
+
+    const [currentDog, changeDog] = useState({
+        status : '',
+        message : ''
+    });
+
+    const [showingImg, setShowingImg] = useState(false);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(dog => {
+            changeDog({...dog});
+            console.log(currentDog)
+            console.log(dog)
+            setShowingImg(true);
+        })
+    },[])
+
+
+    return(
+        <React.Fragment>
+        {(showingImg)?
+        <img src={currentDog.message} alt="A Random Dog"/>
+        : <p>Loading...</p> 
+        }
+        </React.Fragment>
+    )
+};
+
+export default App;
